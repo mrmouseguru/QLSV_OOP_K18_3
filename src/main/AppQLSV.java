6b4772d68f63562a407b53ad98a39ee7a6867023
CGM <mrmouse.guru@gmail.com>
2025-03-26 13:13:57 +0700

    INITIALIZE QUẢN LÝ SINH VIÊN

---
+++ src/main/AppQLSV.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.io.PrintWriter;
+
+public class AppQLSV {
+
+	public static void main(String[] args) {
+
+		//Bước 1
+		PrintWriter out;
+		
+	   //Bước 3     //Bước 2
+		out =       new PrintWriter(System.out);
+		
+		//gửi thông điệp
+		out.print("~~~~ Chương trình QLSV ~~~~");
+		out.flush();
+	}
+
+}
